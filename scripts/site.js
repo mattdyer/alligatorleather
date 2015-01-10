e9400bd3ab031555cfa9d79b1a614af899e48383
mattdyer <madmatt1220@gmail.com>
2015-01-10 13:07:35 -0700

    initial backups added and color image fixes

--- scripts/site.js
+++ scripts/site.js
@@ -1,19 +1,104 @@
 (function($){
 	$(function(){
-		var colorDropdown = $('#select_22');
 		
-		var options = $('option',colorDropdown);
+		var getColorDropdown = function(){
+			var optionLabels = $('.product-options label');
 
-		$('.product-image-thumbs img').each(function(){
-			var img = $(this);
-			var colorName = img.attr('alt');
-			if(colorName.length > 0){
-				var labelContainer = $('<div class="imageLabel">' + colorName + '</div>');
-				img.parent().append(labelContainer);
+			var colorDropdown;
+			var colorLabel;
+
+			optionLabels.each(function(){
+				if($(this).text().match(new RegExp('Color / Finish'))){
+					colorLabel = $(this);
+					return false;
+				}
+			});
+
+			if(colorLabel){
+				colorDropdown = colorLabel.parent().next().find('select');
 			}
-		});
+			return colorDropdown;
+		}
 
-		$('.product-image-thumbs a').click(function(){
+		var makeImagesFromOptions = function(options){
+			
+			var imageThumbContainer = $('.product-image-thumbs');
+			var imageThumbItem = $('.product-image-thumbs li').first();
+
+			var largeImageContainer = $('.product-image-gallery');
+			var largeImage = largeImageContainer.find('img').first();
+
+			largeImageContainer.empty();
+			imageThumbContainer.empty();
+
+			options.each(function(){
+				var option = $(this);
+				var colorName = $.trim(option.text());
+
+				if(imageMap[colorName]){
+					var nextImageThumbItem = imageThumbItem.clone();
+
+					var imageSrc = '/images/color-images/' + imageMap[colorName].filename;
+
+					var thumb = nextImageThumbItem.find('img');
+					thumb.attr('src',imageSrc);
+					thumb.on('load',function(){
+						var imageHeight = $(this).height();
+						$(this).css({'margin':parseInt((75 - imageHeight) / 2,10) + 'px auto'});
+					});
+					thumb.attr('alt',colorName);
+					thumb.removeAttr('height');
+
+					//var imageLabel = nextImageThumbItem.find('.imageLabel');
+					//imageLabel.text(colorName);	
+
+					var labelContainer = $('<div class="imageLabel">' + colorName + '</div>');
+					thumb.parent().append(labelContainer);
+
+					imageThumbContainer.append(nextImageThumbItem);
+
+
+					var nextImage = largeImage.clone();
+
+					nextImage.removeAttr('id');
+					nextImage.attr('src',imageSrc);
+					nextImage.removeClass('visible');
+
+					largeImageContainer.append(nextImage);
+
+					nextImageThumbItem.click(function(){
+						$('.product-image-gallery img').removeClass('visible');
+						nextImage.addClass('visible');
+					});
+				}
+			});
+
+			$('.product-image-thumbs img').first().click();
+		}
+
+		//var colorDropdown = $('#select_22');
+		
+		var colorDropdown = getColorDropdown();
+		if(colorDropdown){
+			var options = $('option',colorDropdown);
+			var imageMap;
+
+			$.get('/images/color-images/map.json',function(data){
+				imageMap = data;
+				makeImagesFromOptions(options);
+			});
+		}
+
+		// $('.product-image-thumbs img').each(function(){
+		// 	var img = $(this);
+		// 	var colorName = img.attr('alt');
+		// 	if(colorName.length > 0){
+		// 		var labelContainer = $('<div class="imageLabel">' + colorName + '</div>');
+		// 		img.parent().append(labelContainer);
+		// 	}
+		// });
+
+		$('.product-image-thumbs').on('click','a',function(){
 			var img = $('img',this);
 
 			var colorName = img.attr('alt');
@@ -27,6 +112,8 @@
 
 			});
 		});
+
+
 
 		/*var ImageMap = {};
 
