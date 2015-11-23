d368b4397c6615866771909cecdd461383fbb3b2
mattdyer <madmatt1220@gmail.com>
2015-11-22 19:57:46 -0700

    changes home belt image

--- magento/app/code/community/MADComputing/QuoteViewer/Block/Adminhtml/QuoteViewerbackend.php
+++ magento/app/code/community/MADComputing/QuoteViewer/Block/Adminhtml/QuoteViewerbackend.php
@@ -2,4 +2,22 @@
 
 class MADComputing_QuoteViewer_Block_Adminhtml_QuoteViewerbackend extends Mage_Adminhtml_Block_Template {
 
+	protected $_quoteCollection = null;
+
+
+	protected function _getCollection()
+    {
+        return  Mage::getResourceModel('reports/quote_collection');
+    }
+
+    public function getCollection()
+    {
+        if (is_null($this->_quoteCollection)) {
+            $this->_quoteCollection = $this->_getCollection();
+            $this->_quoteCollection->prepareForList($this->getCurrentPage());
+        }
+
+        return $this->_quoteCollection;
+    }
+
 }
